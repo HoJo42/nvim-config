fea99d319f663479f6b4fca17cbb1133111ea26d
HoJo42 <howardwalz@gmail.com>
2024-11-22 00:50:59 -0800

    I like relative numbers.

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,7 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-vim.cmd("set number")
+vim.cmd("set relativenumber")
 vim.cmd("set completeopt=menu")
 vim.g.mapleader = " "
 vim.keymap.set("n", "<M-v>", "<C-v>")
